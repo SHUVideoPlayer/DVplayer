c7aa79d1daedf7e215aca30fce7e91bc0af30fb6
Ch-franck <Ch-franck@2823377353@qq.com>
2023-04-18 20:37:55 +0800

    add main and video

--- DVhub/settings.py
+++ DVhub/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DVshow',
-    'InterACT',
-    'Upload',
-    'User',
+
 ]
 
 MIDDLEWARE = [
--- video/apps.py
+++ video/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UploadConfig(AppConfig):
+class VideoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Upload'
+    name = 'video'
