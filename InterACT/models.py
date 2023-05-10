ea95b9fec761f569f9df942483952029074879f7
dika-Paul <2413657071@qq.com>
2023-05-10 16:14:43 +0800

    完成models

--- InterACT/models.py
+++ InterACT/models.py
@@ -1,24 +1,61 @@
-from django.db import models
+import video.models as videomodels
+import User.models as Usermodels
 
 # Create your models here.
 from django.db import models
 
-class Like_Favorite_Table(models.Model):
 
+class Like_Favorite_Table(models.Model):
     type_choices = (("like", "点赞"), ("favorite", "收藏"))
 
-    user = models.CharField(max_length=100)#
-    type = models.CharField(choices=type_choices)
-    DV = models.CharField(max_length=100)#
+    Act_id = models.AutoField(primary_key=True)
+    User = models.Foreignkey(Usermodels.User)
+    Type = models.CharField(choices=type_choices)
+    DV = models.ForeignKey(videomodels.Video)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['User', 'Type', 'DV'],
+                name='unique_act'
+            )
+        ]
+
+    def __str__(self):
+        return self.User + ' ' + self.Type + ' ' + self.DV
+
 
 class Attention_Table(models.Model):
+    Attention_id = models.AutoField(primary_key=True)
+    User = models.Foreignkey(Usermodels.User)
+    Up = models.Foreignkey(Usermodels.User)
 
-    user = models.CharField(max_length=100)#
-    up = models.CharField(max_length=100)#
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['User', 'Up'],
+                name='unique_act'
+            )
+        ]
+
+    def __str__(self):
+        return self.User + ' ' + self.Up
+
 
 class Comment_Table(models.Model):
+    Comment_id = models.AutoField(primary_key=True)
+    User = models.Foreignkey(Usermodels.User)
+    Comment = models.CharField(max_length=None, verbose_name="评论")
+    Time = models.TimeField(auto_now_add=True, verbose_name="创建时间")
+    DV = models.ForeignKey(videomodels.Video)
 
-    user = models.CharField(max_length=100)#
-    comment = models.CharField(max_length=None, verbose_name="评论")
-    time = models.TimeField(auto_now_add=True, verbose_name="创建时间")
-    DV = models.CharField(max_length=100)#
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['User', 'Comment', 'Time', 'DV'],
+                name='unique_act'
+            )
+        ]
+
+    def __str__(self):
+        return self.DV + ' ' + self.User + ':' + self.Comment
--- InterACT/views.py
+++ InterACT/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def view_test(request):
     return render(request, 'Comment_view.html')
+
+def comment_view(request):
+    return render(request, 'Comment_view.html')
